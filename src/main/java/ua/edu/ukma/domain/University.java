7a85169143546b05f13b4490fa09836a5fb4256a
Illia Kabysh <recyut5132@gmail.com>
2026-02-07 18:02:54 +0200

    Add domain classes

---
+++ src/main/java/ua/edu/ukma/domain/University.java
@@ -0,0 +1,15 @@
+package ua.edu.ukma.domain;
+
+class University {
+    private String fullName;
+    private String shortName;
+    private String city;
+    private String address;
+    public University(String fullName, String shortName, String city, String address) {
+        this.fullName = fullName;
+        this.shortName = shortName;
+        this.city = city;
+        this.address = address;
+    }
+
+}
