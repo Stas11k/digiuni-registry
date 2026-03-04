4401351e12c2dae677d107c3ed7fd14d6676646a
Illia Kabysh <recyut5132@gmail.com>
2026-03-04 12:54:49 +0200

    fix repo and service

--- src/test/java/ua/edu/ukma/service/StudentServiceTest.java
+++ src/test/java/ua/edu/ukma/service/StudentServiceTest.java
@@ -3,7 +3,6 @@ package ua.edu.ukma.service;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ua.edu.ukma.domain.*;
-import ua.edu.ukma.repository.InMemoryStudentRepository;
 
 import static org.junit.jupiter.api.Assertions.*;
 
